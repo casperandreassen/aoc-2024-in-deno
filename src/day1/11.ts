80496522f4e598de4d74ecf7c663b4fc29088024
casper <c@andreassen.dev>
2024-11-28 09:43:38 +0100

    Inital commit. Added VSCODE launch settings for running the problems. Lets see if this is the final structure.

---
+++ src/day1/11.ts
@@ -0,0 +1,7 @@
+export function _11() {
+  console.log("You ran 11");
+}
+
+if (import.meta.main) {
+  _11();
+}
---
+++ src/day1/12.ts
@@ -0,0 +1,7 @@
+export function _12() {
+  console.log("You ran 12");
+}
+
+if (import.meta.main) {
+  _12();
+}
